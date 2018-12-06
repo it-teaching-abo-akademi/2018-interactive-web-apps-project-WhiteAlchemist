6916f5809f96ae0411282f8b94deb8d96b819817
WhiteAlchemist <enushi5@gmail.com>
2018-12-06 16:06:48 +0200

    Updates

---
+++ precache-manifest.9b25251126fa234d381a0f0641f6fd09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/2018-interactive-web-apps-project-WhiteAlchemist/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d20d4b35a6b416e0c63e",
+    "url": "/2018-interactive-web-apps-project-WhiteAlchemist/static/js/runtime~main.d20d4b35.js"
+  },
+  {
+    "revision": "3f96310ad8c3efe1b576",
+    "url": "/2018-interactive-web-apps-project-WhiteAlchemist/static/js/main.3f96310a.chunk.js"
+  },
+  {
+    "revision": "dcd800a0b568476ff982",
+    "url": "/2018-interactive-web-apps-project-WhiteAlchemist/static/js/1.dcd800a0.chunk.js"
+  },
+  {
+    "revision": "3f96310ad8c3efe1b576",
+    "url": "/2018-interactive-web-apps-project-WhiteAlchemist/static/css/main.3c85c982.chunk.css"
+  },
+  {
+    "revision": "c720ea1d0a83f3837ea26a88e5b6a67f",
+    "url": "/2018-interactive-web-apps-project-WhiteAlchemist/index.html"
+  }
+];
