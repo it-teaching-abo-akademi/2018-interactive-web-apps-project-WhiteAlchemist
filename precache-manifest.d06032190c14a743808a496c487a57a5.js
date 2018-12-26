54d024abcf45054cef31aee6d75a0267fe7465c5
WhiteAlchemist <enushi5@gmail.com>
2018-12-26 10:54:30 +0100

    Updates

--- precache-manifest.d06032190c14a743808a496c487a57a5.js
+++ precache-manifest.d06032190c14a743808a496c487a57a5.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/2018-interactive-web-apps-project-WhiteAlchemist/static/js/runtime~main.d20d4b35.js"
   },
   {
-    "revision": "6a6962371c4cb0882251",
-    "url": "/2018-interactive-web-apps-project-WhiteAlchemist/static/js/main.6a696237.chunk.js"
+    "revision": "a44a765f698bff4057b6",
+    "url": "/2018-interactive-web-apps-project-WhiteAlchemist/static/js/main.a44a765f.chunk.js"
   },
   {
     "revision": "82f511c9c15aacbf6ba3",
     "url": "/2018-interactive-web-apps-project-WhiteAlchemist/static/js/1.82f511c9.chunk.js"
   },
   {
-    "revision": "6a6962371c4cb0882251",
+    "revision": "a44a765f698bff4057b6",
     "url": "/2018-interactive-web-apps-project-WhiteAlchemist/static/css/main.0de9d0ae.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/2018-interactive-web-apps-project-WhiteAlchemist/static/css/1.b6dbde93.chunk.css"
   },
   {
-    "revision": "83c6c96a3a7a9a2893229b3b9b14f96b",
+    "revision": "f171f291213bed1727dc61bfe1913955",
     "url": "/2018-interactive-web-apps-project-WhiteAlchemist/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/2018-interactive-web-apps-project-WhiteAlchemist/precache-manifest.ecfb972454e3eee9ea66204a5ff22fed.js"
+  "/2018-interactive-web-apps-project-WhiteAlchemist/precache-manifest.d06032190c14a743808a496c487a57a5.js"
 );
 
 workbox.clientsClaim();
